d981c7a0054286338779419f6e4dff16ed6d9162
Brian <brianlin7453@gmail.com>
2020-01-08 20:26:20 -0500

    Fixed please typo

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -37,7 +37,7 @@ const CheckoutPage = ({ cartItems, total }) => (
             <span>TOTAL: ${total}</span>
         </div>
         <div className='test-warning'>
-        Plese user the following test credit card for payment*
+        Please user the following test credit card for payment*
         <br />
         4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
