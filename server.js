7426ecb6c56af0f6e32c19eb5b8b170fa6718fcc
Brian <brianlin7453@gmail.com>
2020-03-06 15:46:10 -0500

    Fixing heroku app crash

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //bodyParser middleware
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -20,10 +21,8 @@ app.use(
 );
 app.use(cors());
 
-
 if (process.env.NODE_ENV === "production") {
   app.use(compression());
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
@@ -33,10 +32,6 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
   console.log(`Server running on port ${port}`);
-});
-
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.post("/payment", (req, res) => {
