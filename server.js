ad21a8434e45962a63ac5532d14dc05d7615f761
Brian <brianlin7453@gmail.com>
2020-03-06 15:31:40 -0500

    Added app.use(compression()); to production if statement

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //bodyParser middleware
-app.use(compression());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -23,6 +22,7 @@ app.use(cors());
 
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function(req, res) {
